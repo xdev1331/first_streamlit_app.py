37457b694a365f1e299b21de9e135017fbb4ad44
xdev1331 <131064070+xdev1331@users.noreply.github.com>
2023-04-18 12:05:22 -0700

    Update streamlist_app.py
    Fix bug in code, steamlit -->streamlit

--- streamlist_app.py
+++ streamlist_app.py
@@ -4,8 +4,8 @@ import streamlit
 
 streamlit.title("My parents New Healthy Diner")
 
-steamlit.header("Breakfast Menu")
-steamlit.text("Omega 3 & Blueberry Oatmeal")
-steamlit.text("Kale, Spinach & Rocket Smoothie")
-steamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
 
