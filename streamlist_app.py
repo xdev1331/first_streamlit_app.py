3b0c714018f66de9d63ea1eb61084ca8500e92f8
xdev1331 <131064070+xdev1331@users.noreply.github.com>
2023-04-18 14:40:23 -0700

    Update streamlist_app.py
    added multiselect

--- streamlist_app.py
+++ streamlist_app.py
@@ -20,4 +20,8 @@ filepathname = asw_S3_dabw + filename
 
 my_fruit_list = pd.read_csv(filepathname)
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
