809e57576df1100284ec57a4f62ec0123079267d
xdev1331 <131064070+xdev1331@users.noreply.github.com>
2023-04-18 12:04:28 -0700

    Update streamlist_app.py
    Added Title, and text

--- streamlist_app.py
+++ streamlist_app.py
@@ -4,3 +4,8 @@ import streamlit
 
 streamlit.title("My parents New Healthy Diner")
 
+steamlit.header("Breakfast Menu")
+steamlit.text("Omega 3 & Blueberry Oatmeal")
+steamlit.text("Kale, Spinach & Rocket Smoothie")
+steamlit.text("Hard-Boiled Free-Range Egg")
+
