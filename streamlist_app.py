802f1b4a9bee417ae644892a65278b50376ec34d
xdev1331 <131064070+xdev1331@users.noreply.github.com>
2023-04-21 12:09:47 -0700

    Update streamlist_app.py
    fix requests bug

--- streamlist_app.py
+++ streamlist_app.py
@@ -38,6 +38,6 @@ streamlit.dataframe(fruits_to_show)
 
 
 # use api calls
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
